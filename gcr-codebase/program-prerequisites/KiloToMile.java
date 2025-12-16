0f76a75ff8bfac30c0af564f3363923beed67e35
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-16 19:51:57 +0530

    [Shreya] : Added Kilometer To Mile converter

---
+++ gcr-codebase/program-prerequisites/KiloToMile.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class KiloToMile
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter distance in Kilometres");
+a=sc.nextInt();
+
+
+System.out.println("Distance in Miles:"+ a* 0.621371);
+}
+}
