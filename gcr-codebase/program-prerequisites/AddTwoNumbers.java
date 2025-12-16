69193f2afb414d23968120b5b266bf505c9734a1
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-16 19:42:09 +0530

    [Shreya Shristi] : Added AddTwoNumbers

---
+++ gcr-codebase/program-prerequisites/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddTwoNumbers
+{
+ public static void main(String args[])
+ {
+  int a,b;
+  Scanner sc=new Scanner(System.in);
+  a=sc.nextInt();
+  b=sc.nextInt();
+
+  System.out.println(a+b);
+ }
+}
