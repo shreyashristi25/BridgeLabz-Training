ece1a5f84c437a5ded4fd44997d4879fafad9604
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-17 20:31:01 +0530

    [Shreya] : Added program to calculate area of traingle

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/AreaOfTriangle.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+
+//create class AreaOfTriange to calculate volume of earth
+public class AreaOfTriangle {
+   public static void main (String args []) {
+   Scanner input = new Scanner(System.in) ;
+   
+   
+   //create variable base and height 
+   double base ;
+   double height ;
+   double areaInCentimeters ;
+   double areaInInches ;
+   
+   //Taking user input for base and height
+   System.out.println("Enter the base of the triangle") ;
+   base = input.nextDouble() ;
+   System.out.println("Enter the height of the triangle") ;
+   height = input.nextDouble() ;
+   
+   // calculating area of triangle
+   areaInInches = 0.5 * base * height * 2.54 ;
+   areaInCentimeters = 0.5 * base * height ;
+   
+   //Displaying the area of triangle in inches and centimeters
+   System.out.println("Area of triangle in square inches is " +areaInInches+" and in square centimeters is " +areaInCentimeters) ;
+   
+   
+   }
+}
