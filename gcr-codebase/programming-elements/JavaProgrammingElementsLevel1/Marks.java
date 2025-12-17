f1ba25e350a52e666ece7e9c2f323c368473388e
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-17 16:13:46 +0530

    [Shreya] : Added average percent calculator program

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/Marks.java
@@ -0,0 +1,16 @@
+//create a class Marks to calculate the total marks of Sam
+public class Marks {
+   public static void main ( String args[] ) {
+   
+      //create variables to assign marks of different subjects
+      int maths = 94 ;
+	  int physics = 95 ;
+	  int chemistry = 96 ;
+	  
+	  //create variable to calculate total marks of the subjects
+	  int totalMarks = (maths + physics + chemistry) / 3  ;
+	  
+	  //Displaying the average percent mark of Sam
+	  System.out.println ( "Sam's average mark in PCM is" + " " +totalMarks) ;
+   }
+ }
