3c2ab0c2656319cd6159b04938a0dea58b0abe2e
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-17 19:58:20 +0530

    [Shreya] : Added HeightConverter program

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/HeightConverter.java
@@ -0,0 +1,24 @@
+import java.util.* ;
+//create a HiegitConverter class to convert height in inches and feet
+public class HeightConverter {
+   public static void main(String args[]) {
+   Scanner input = new Scanner(System.in) ;
+   
+   //create variable to store height in centimeters, foot and inches
+   double height ;
+   double foot ;
+   double inches ;
+   
+   //Taking user height as input
+   System.out.println("Enter your height in centimeters") ;
+   height = input.nextDouble() ;
+   
+   //computing height
+   inches = 2.54 * height ; 
+   foot = inches * 12 ;
+   
+   //Displaying height in feet annd inches
+   System.out.println(" Your Height in cm is " +height +" while in feet is " +foot +" and in inches is " +inches) ; 
+   
+   }
+}
