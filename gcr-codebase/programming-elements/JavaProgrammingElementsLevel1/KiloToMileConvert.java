d63257eac0f72004df4c521ce1723718f1ba41ed
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-17 17:28:46 +0530

    [Shreya] : Added Km to mile converter by taking user input

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/KiloToMileConvert.java
@@ -0,0 +1,17 @@
+import java.util.*;
+//create KiloToMileConvert class to take user input in kilometers and change it to miles
+public class KiloToMileConvert {
+   public static void main (String args[]) {
+      Scanner input = new Scanner(System.in) ;
+	  
+	  //create variables to store distance in kilometers and miles
+      double km ;
+      double miles ;
+      km = input.nextInt() ;
+      miles = km * 1.6 ;
+	  
+	  //Displaying distance in kilometers and miles 
+	  System.out.println(" The total miles is " +miles +" mile for the given " +km+ " km") ;
+   
+   }
+}
