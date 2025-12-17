47c6f4d751eb39cdcbe53f1dc034ca96919e36f7
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-17 16:15:41 +0530

    [Shreya] : Added Kilometers to miles converter

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/KilometersToMiles.java
@@ -0,0 +1,12 @@
+//create class KilometersToMiles to convert distance in kilometers to miles
+public class KilometersToMiles {
+    public static void main (String args[]) {
+	   
+	   //creating variables to store distance in km and distance in miles 
+       double distance = 10.8 ;
+	   double distanceInMiles = distance * 1.6 ;
+	   
+	   //Displaying distance in miles
+	   System.out.println( "The distance 10.8 km in miles is " + distanceInMiles) ;
+	}
+}
