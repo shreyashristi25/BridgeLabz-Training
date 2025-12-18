38df1a8bfd9396ce5ba6e2288eeb46fa49a9202c
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-18 20:43:18 +0530

    [Shreya] : Implemented Factorial

---
+++ gcr-codebase/control-flow/JavaControlFlowLevel1/Factorial.java
@@ -0,0 +1,28 @@
+
+import java.util.* ;
+public class Factorial {
+   public static void main (String args[]) {
+      Scanner input = new Scanner (System.in) ;
+	  
+	  //creating varibale number and getting user input
+	  int number ;
+	  System.out.println("Enter the number : ") ;
+	  number = input.nextInt() ;
+	  int factorial = 1 ;
+	  int num = number ;
+
+      //computing factorial of number
+      if (number > 0) {
+	     while(num >= 1) {
+		    factorial = factorial * num ;
+			num -- ;
+		 }
+		 System.out.println("The factorial of " +number +" is " +factorial) ;
+	  }	 
+      else {
+	     System.out.println ("Invalid input ") ;
+	  }	
+      input.close() ; 	  
+	 
+   }
+}
