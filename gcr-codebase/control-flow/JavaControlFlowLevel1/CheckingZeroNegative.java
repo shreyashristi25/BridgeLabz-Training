e9d090cfdc760adadf172b8095c7c663af0f8bc5
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-18 18:22:25 +0530

    [Shreya] : Implemented CheckingZeroNegative to sum until user enters 0 or negative

---
+++ gcr-codebase/control-flow/JavaControlFlowLevel1/CheckingZeroNegative.java
@@ -0,0 +1,26 @@
+
+import java.util.* ;
+public class CheckingZeroNegative {
+   public static void main (String args[]) {
+      Scanner input = new Scanner (System.in) ;
+	  
+	  //creating variables total and assign 0.0, create variable value and take user input
+	  double total = 0.0 ;
+	  double value ;
+	  System.out.println("Enter a value : ") ;
+	  value = input.nextDouble() ;
+	  
+	  //computing total until user enters 0
+	  
+	     while(value != 0.0 || value < 0.0 ) {
+	     total = total + value ;
+	     System.out.println("Enter a value again") ;
+		 value = input.nextDouble() ;
+		 
+		 }
+		
+	  System.out.println("Total sum of numbers : " +total) ;
+	  
+	  input.close();
+   }
+}
