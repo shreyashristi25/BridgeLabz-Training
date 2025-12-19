bde933078789567a8748e02c6372eaa62d2ff05b
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-19 10:53:01 +0530

    [Shreya] : Implemented LeapYearUsinfAndOR using && and ||

---
+++ gcr-codebase/control-flow/JavaControlFlowLevel3/LeapYearUsingAndOr.java
@@ -0,0 +1,25 @@
+
+import java.util.* ;
+public class LeapYearUsingAndOr {
+	public static void main (String args[]) {
+		Scanner input = new Scanner(System.in) ;
+		
+		//creating variable to store year
+		System.out.println("Enter a year : ") ;
+		int year = input.nextInt() ;
+			
+		//checking for leap year using only one if statement and multiple logical statements
+		if(year >= 1582) {
+		if( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+			System.out.println (year +" is a leap year" +"-using single if ") ;
+		}
+		else {
+			System.out.println (year +" is  not a leap year" +"-using single if") ;
+		}
+	    }
+		else {
+			System.out.println ("Invalid input : Please eneter a year >= 1582") ;
+		}
+		
+	}
+}
