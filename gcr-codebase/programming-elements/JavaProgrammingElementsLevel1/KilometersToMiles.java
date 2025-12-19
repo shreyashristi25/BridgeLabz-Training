87c543ff7822575dc98011cab8be18cbb5371342
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-19 11:47:31 +0530

    [Shreya] : Corrected formula for KilometersToMiles

--- gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/KilometersToMiles.java
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/KilometersToMiles.java
@@ -4,7 +4,7 @@ public class KilometersToMiles {
 	   
 	   //creating variables to store distance in km and distance in miles 
        double distance = 10.8 ;
-	   double distanceInMiles = distance * 1.6 ;
+	   double distanceInMiles = distance * 0.621371 ;
 	   
 	   //Displaying distance in miles
 	   System.out.println( "The distance 10.8 km in miles is " + distanceInMiles) ;
