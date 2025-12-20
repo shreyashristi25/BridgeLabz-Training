c771fe991c8c2231b044df035f3909756799dac9
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-20 16:54:38 +0530

    [Shreya] : Added SmallestAndLargest with comments

--- gcr-codebase/methods/SmallestAndLargest.java
+++ gcr-codebase/methods/SmallestAndLargest.java
@@ -1,6 +1,8 @@
 
 import java.util.* ;
 public class SmallestAndLargest {
+	
+	//creating a method findSmallestAndLargest to find smallest and largest among three numbers
 	public static int[] findSmallestAndLargest(int number1, int number2, int number3) {
 	 int [] arr = new int[2] ;
 	 if(number1 > number2 && number1 > number3) {
@@ -24,6 +26,8 @@ public class SmallestAndLargest {
 	  return arr  ;
 	}
 	public static void main (String args []) {
+		
+		//creating varibale number1, number2 and number3 and getting user input and printing the largest and second largest
 		Scanner input = new Scanner (System.in) ;
 		int number1, number2, number3 ;
 		System.out.println ("Enter the first number : ") ;
