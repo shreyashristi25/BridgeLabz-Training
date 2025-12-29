87ed27d84fdd885905aeab89710cbdd08936a516
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-29 13:26:38 +0530

    [Shreya] : Updated the logic for GuessGame

--- scenario-based-codebase/GuessGame.java
+++ scenario-based-codebase/GuessGame.java
@@ -8,29 +8,36 @@ public class GuessGame {
 		boolean guessed = false ;
 		
 		do {
-			System.out.println("Think of a number between 1 and 100") ;
-		System.out.println("I will guess the number") ;
+			System.out.println("Let's guess a number between 1 and 100") ;
+			System.out.println() ;
+			System.out.println("Enter a number between 1 and 100" ) ;
+			int number = sc.nextInt() ;
+			 
+			Random random = new Random() ;
+			int guessNumber =  random.nextInt(100) + 1 ;
+			System.out.println("The number i guessed is :" +guessNumber) ;
 		
-		Random random = new Random() ;
-		int guessNumber =  random.nextInt(100) + 1 ;
-		System.out.println("The number is :" +guessNumber) ;
-		
-		System.out.println("Was my guess correct/low/high ?") ;
-		String feedback = sc.next() ;
-		
-		if(feedback.equalsIgnoreCase("Correct")) {
-			guessed = true ;
-			System.out.println("Yay! My guess was correct.") ;
-		}
-		else {
-			System.out.println("Oh! Let's Try Again.") ;
-		}
-		attempt++ ;
+			if(number == guessNumber) {
+				guessed = true ;
+				System.out.println() ;
+				System.out.println("Yay! You guessed correct.") ;
+			}
+			else if(number < guessNumber){
+				System.out.println() ;
+				System.out.println("Too low!") ;
+				System.out.println("Let's Try Again!") ;
+			}
+			else {
+				System.out.println() ;
+				System.out.println("Too High!") ;
+				System.out.println("Let's Try Again!") ;
+			}
+			attempt++ ;
 		}
 		while(attempt <= 5 && !guessed) ;
 		if(!guessed) {
 			System.out.println("Was not able to guess in 5 attempts :( ") ;
-			System.out.println("I will try next time!") ;
+			System.out.println("Better luck next time!") ;
 		}
 		
 		sc.close() ;
