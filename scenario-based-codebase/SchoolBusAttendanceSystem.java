1ae381717d7a49fdd749eaca812fcbdf28f6180a
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-29 21:01:21 +0530

    [Shreya] : Updated SchoolBusAttendanceSystem with comments

--- scenario-based-codebase/SchoolBusAttendanceSystem.java
+++ scenario-based-codebase/SchoolBusAttendanceSystem.java
@@ -1,9 +1,15 @@
+/* School Bus Attendance System 🚍
+Track 10 students' presence.
+● Use for-each loop on names.
+● Ask "Present or Absent?"
+● Print total present and absent counts.*/
 
 import java.util.* ;
 public class SchoolBusAttendanceSystem {
 	public static void main (String args []) {
 		Scanner sc = new Scanner(System.in) ;
 		
+		//Storing the names of students in a string array
 		String [] students ={"Shreya", "Priya", "Rishabh", "Satyam", "Vishal", "Riya", "Yash", "Rahul", "Krish", "Shruti"} ;
 		
 		String s = "" ;
@@ -11,6 +17,8 @@ public class SchoolBusAttendanceSystem {
 		int absentCount = 0;
 		System.out.println("----------Taking Attendance---------") ;
 		System.out.println() ;
+		
+		//Calling for studnets and checking if present or absent
 		for(int i=0; i < students.length; i++) {
 			System.out.println(students[i]+" " +"(Present or Absent)?") ;
 			String s1 =  sc.next();
@@ -22,6 +30,8 @@ public class SchoolBusAttendanceSystem {
 				absentCount++ ;
 			}
 		}
+		
+		//Dispalying the total number of present and absent students
 		System.out.println("The number of students present is :" + presentCount); 
 		System.out.println("The number of students absent is :" + absentCount); 
 		sc.close() ;
