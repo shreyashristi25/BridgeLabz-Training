8b5c492bb4f61ed0f89d223640a22ad5b0e46b3d
shreyashristi25 <shreyashristi25@gmail.com>
2025-12-30 22:51:00 +0530

    [Shreya] : Added Procut file for E-commerce Platform

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Product.java
@@ -0,0 +1,25 @@
+package com.objectmodeling;
+
+public class Product {
+
+	private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayProduct() {
+        System.out.println("Product: " + name + " | Price: " + price);
+    }
+
+}
