d42d1623de09cfdb7643083fb3363c60252ea5a0
shreyashristi25 <shreyashristi25@gmail.com>
2026-01-03 20:11:38 +0530

    [Shreya] : Added two files for Longest Consecutive Sequence

---
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmaps/longestconsecutivesequence/LongestConsecutiveMain.java
@@ -0,0 +1,30 @@
+package com.hashmaps.longestconsecutivesequence;
+import java.util.* ;
+
+//driver class for LongestConsecutiveSequence class 
+public class LongestConsecutiveMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		//creating object, accessing methods and displaying result
+		LongestConsecutiveSequence l = new LongestConsecutiveSequence() ;
+		
+		//Taking user input 
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = l.longestConsecutive(nums);
+        System.out.println("Length of longest consecutive sequence: " + result);
+
+        sc.close();
+
+
+	}
+
+}
---
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmaps/longestconsecutivesequence/LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+package com.hashmaps.longestconsecutivesequence;
+import java.util.* ;
+public class LongestConsecutiveSequence {
+
+	//method to find longest consecutive sequence
+	public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+
+        return longest ;
+	}
+}
