0f42dc03f9e5f558d1641b4dea0c85ea35917572
shreyashristi25 <shreyashristi25@gmail.com>
2026-01-03 20:21:58 +0530

    [Shreya] : Updated logic for LongestConsecutive

--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmaps/longestconsecutivesequence/LongestConsecutiveSequence.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmaps/longestconsecutivesequence/LongestConsecutiveSequence.java
@@ -3,7 +3,7 @@ import java.util.* ;
 public class LongestConsecutiveSequence {
 
 	//method to find longest consecutive sequence
-	public static int longestConsecutive(int[] nums) {
+	public static int longestConsecutive(int [] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
