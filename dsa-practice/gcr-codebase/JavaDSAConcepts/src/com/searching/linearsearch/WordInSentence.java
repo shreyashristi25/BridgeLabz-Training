aaa6792cd04aa082e4b96dab5877610a0e0bd39d
shreyashristi25 <shreyashristi25@gmail.com>
2026-01-07 11:17:47 +0530

    [Shreya] : Modified WordInSentence file

--- dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/searching/linearsearch/WordInSentence.java
+++ dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/searching/linearsearch/WordInSentence.java
@@ -1,9 +1,10 @@
 package com.searching.linearsearch;
 import java.util.* ;
 
-public class WordInScentence {
+public class WordInSentence {
 	
 		
+	//method to check if the sentences contain the given word 
 		public static String findWord(String [] sentences, String word) {
 			
 			for(String sentence : sentences) {
@@ -14,9 +15,11 @@ public class WordInScentence {
 			}
 			return "Not Found" ;
 		}
+		
 		public static void main(String args []) {
 			Scanner sc = new Scanner (System.in) ;
 		
+			//Taking user input and displaying result
 			System.out.println("Enter the number of sentences :") ;
 			int size = sc.nextInt();
 			
@@ -32,5 +35,7 @@ public class WordInScentence {
 			
 			String result = findWord(sentences, word) ;
 			System.out.println("Word found in the sentences ? " +result) ;
+			
+			sc.close();
 	}
 }
