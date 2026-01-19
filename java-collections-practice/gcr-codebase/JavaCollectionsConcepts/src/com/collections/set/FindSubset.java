770a5336d28c61eff94f896e28c7de4eae9c2414
shreyashristi25 <shreyashristi25@gmail.com>
2026-01-19 22:43:32 +0530

    [Shreya] : Implemented FindSubset to check if one set is subset of another set

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/collections/set/FindSubset.java
@@ -0,0 +1,38 @@
+package com.collections.set;
+import java.util.* ;
+
+public class FindSubset {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in) ;
+		
+		Set<Integer> set1 = new HashSet<>() ;
+		Set<Integer> set2 = new HashSet<>() ;
+		
+		//taking user input for number of elements in two sets and their elements
+		System.out.println("Enter number of elements in set1 :") ;
+		int n1 =sc.nextInt() ;
+		sc.nextLine() ;
+				
+		System.out.println("Enter the elements of set1 : ") ;
+		for( int i =0; i <n1; i++) {
+			set1.add(sc.nextInt()) ;
+		}
+				
+		System.out.println("Enter number of elements in set2 :") ;
+		int n2 =sc.nextInt() ;
+		sc.nextLine() ;
+				
+		System.out.println("Enter the elements of set2 : ") ;
+		for( int i = 0; i <n2; i++) {
+			set2.add(sc.nextInt()) ;
+		}
+		
+		//checking for subset and displaying result
+		boolean isSubset = set2.containsAll(set1) ;
+		
+		System.out.println("Set1 : " +set1) ;
+		System.out.println("Set2 : " +set2) ;
+		System.out.println("Is set1 Subset of set2 ? " +isSubset) ;
+	}
+}
